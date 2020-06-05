02010a5e04781814320420c30341f4c935cd553e
AppantasyArthur <appantasy.com@gmail.com>
2020-06-05 17:18:58 +0800

    Display component main done

---
+++ src/components/Display/Display.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Display = ({ displayValue }) => <div className='display-container' />;
+Display.prototype = { displayValue: PropTypes.string.isRequired };
+export default Display;
